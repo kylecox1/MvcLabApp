70a13ee66f63bad625952957db11bb5a2c20a7ca
kylecox1 <33430803+kylecox1@users.noreply.github.com>
2022-01-13 21:27:40 -0500

    Calculator function

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,15 +12,68 @@ namespace MvcLabApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private ResultViewModel _messageViewModel;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _messageViewModel = new ResultViewModel();
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Result(CalculatorViewModel model, string operation)
+        {
+            switch (operation)
+            {
+                case "Plus":
+                    return RedirectToAction("Plus", new { num1 = model.Num1, num2 = model.Num2 });
+                case "Minus":
+                    return RedirectToAction("Minus", new { num1 = model.Num1, num2 = model.Num2 });
+                case "Multiply":
+                    return RedirectToAction("Multiply", new { num1 = model.Num1, num2 = model.Num2 });
+                case "Divide":
+                    return RedirectToAction("Divide", new { num1 = model.Num1, num2 = model.Num2 });
+            }
+
+            return Content($"Your answer is {model.Num1}");
+        }
+
+        public IActionResult Plus(double num1, double num2)
+        {
+            double result = num1 + num2;
+            _messageViewModel.Message = $"The result is: {result}";
+            return View("Message", _messageViewModel);
+            ;
+        }
+
+        public IActionResult Minus(double num1, double num2)
+        {
+            double result = num1 - num2;
+            _messageViewModel.Message = $"The result is: {result}";
+            return View("Message", _messageViewModel);
+            ;
+        }
+
+        public IActionResult Multiply(double num1, double num2)
+        {
+            double result = num1 * num2;
+            _messageViewModel.Message = $"The result is: {result}";
+            return View("Message", _messageViewModel);
+            ;
+        }
+
+        public IActionResult Divide(double num1, double num2)
+        {
+            double result = num1 / num2;
+            _messageViewModel.Message = $"The result is: {result}";
+            return View("Message", _messageViewModel);
+            ;
         }
 
         public IActionResult Privacy()
---
+++ Models/CalculatorViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcLabApp.Models
+{
+    public class CalculatorViewModel
+    {
+        [Display(Name = "First Number")]
+        public double Num1 { get; set; }
+
+
+        [Display(Name = "Second Number")]
+        public double Num2 { get; set; }
+
+        public string Operation { get; set; }
+    }
+}
